a5809a02fafa950958215137900b6e933c9c0ae8
Arpit Tyagi <arpittyagi102@gmail.com>
2023-07-21 16:23:21 +0530

    added al 4 APIs

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ MongoClient.connect(MONGO_URI, { useUnifiedTopology: true })
  
 app.post('/', (req, res) => {
     const shortUrl = random();
-    const { url } = req.body;
-    if (url) {
-        db.collection('urlmap').insertOne({ url, shortUrl });
+    const { url, expiry } = req.body;
+    if (url && expiry) {
+        db.collection('urlmap').insertOne({ url, shortUrl,expiry });
         res.json({ "message": "URL shortened successfully", shortUrl });
     }
     else {
@@ -34,17 +34,60 @@ app.post('/', (req, res) => {
 
 app.get('/:id',async (req, res) => {
     const { id } = req.params;
-    
+    console.log(id);
     const all = db.collection('urlmap');
-    const data = await all.findOne({ shortUrl:"5d4ugv8" });
+    const data = await all.findOne({ shortUrl:id });
 
     if(!data){
         return res.json({"message":"Not found"});
     } 
+
+    if (data.expiryDate && new Date(data.expiryDate) < new Date()) {
+        return res.json({ "message": "Short URL has expired" });
+    }
   
     res.redirect(data.url);
   }
 );
+
+app.post('/update', async (req, res) => {
+    const { shortUrl, newUrl } = req.body;
+    if (!shortUrl || !newUrl) {
+        return res.status(400).json({ "message": "complete data not found" });
+    }
+
+    const existingUrl = await db.collection('urlmap').findOne({ shortUrl });
+    if (!existingUrl) {
+        return res.status(404).json({ "message": "Short URL not found in db" });
+    }
+
+    db.collection('urlmap').updateOne({ shortUrl }, { $set: { url: newUrl } });
+    res.json({ "message": "Destination URL updated successfully", shortUrl, newUrl });
+});
+
+app.post('/updateExpiry', async (req, res) => {
+    const { shortUrl, daysToAdd } = req.body;
+
+    if (!shortUrl || !daysToAdd) {
+        return res.status(400).json({ "message": "complete data not found" });
+    }
+
+    const existingUrl = await db.collection('urlmap').findOne({ shortUrl });
+
+    if (!existingUrl) {
+        return res.status(404).json({ "message": "Short URL not found in db" });
+    }
+
+    const newExpiry = existingUrl.expiry + daysToAdd;
+
+    await db.collection('urlmap').updateOne(
+        { shortUrl },
+        { $set: { expiry: newExpiry } }
+    );
+
+    res.json({ "message": "Expiry date updated successfully", expiry });
+});
+
 
 function random() {
     AllCharactors = "abcdefghijklmnopqrstuvwxyz0123456789";
