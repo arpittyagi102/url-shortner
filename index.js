ac42dcd4e969163b22225e1e22a7ce9fa50fb83a
Arpit Tyagi <arpittyagi102@gmail.com>
2023-07-21 14:40:42 +0530

    added a random function

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express');
-const app = express()
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 4000;
 
-app.get('/',(req,res)=>{
-    res.json({"message":"hello"})
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log(random());
+    if (req.body.url) {
+        const { url } = req.body;
+        res.json({ "message": "hello", url })
+    }
+    else {
+        res.status(400).json({ "message": "URL not found" })
+    }
 })
 
-app.listen(4000);
+function random() {
+    AllCharactors = "abcdefghijklmnopqrstuvwxyz0123456789";
+    var random = '';
+
+    for (let i = 0; i < 7; i++) {
+        const randomValue = Math.floor(Math.random() * AllCharactors.length);
+        random += AllCharactors[randomValue];
+    }
+
+    return random;
+}
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+});
